7a41534c5660527ce3f2dd6c393648e40f6ab718
subham-proj <3133ss32@gmail.com>
2021-07-16 21:28:42 +0530

    dev5.2

--- src/components/heatmap.js
+++ src/components/heatmap.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "react-bootstrap";
 import CalendarHeatmap from "react-calendar-heatmap";
 
 import moment from "moment";
+
+// function to re render with change in year
+function useForceUpdate() {
+  let [value, setState] = useState(true);
+  return () => setState(!value);
+}
 
 function Heatmap({ data }) {
   // console.log(data);
   const countData = [];
   const year = [];
 
-  let y = new Date().getFullYear();
-  let selectedYear = y.toString();
+  let currentYear = new Date().getFullYear().toString();
 
   if (data.length >= 1) {
     const arr = [];
@@ -20,7 +25,7 @@ function Heatmap({ data }) {
       let temp = moment.unix(data[i].creationTimeSeconds).format("YYYY-MM-DD");
       let years = moment.unix(data[i].creationTimeSeconds).format("YYYY");
       arr.push(temp);
-      x.push(years);
+      if (years !== currentYear) x.push(years);
     }
 
     // mapping dates with total number of submissions
@@ -49,26 +54,41 @@ function Heatmap({ data }) {
     // console.log(countData[0]);
   }
 
-  const handleChange = (event) => {
-    event.preventDefault();
-    selectedYear = event.target.value;
-    console.log(selectedYear);
-  };
+  const [selectedYear, setselectedYear] = useState("");
 
+  useForceUpdate(); //this function forces to render and we'll be useful when we have changes the year option
+
+  let displayYear = currentYear;
+  // handling the case when a user is searched and has an year and the next user doesn't have it so it give blank graph
+  for (let i = 0; i < year.length; i++) {
+    if (selectedYear) {
+      if (selectedYear === year[i]) {
+        displayYear = selectedYear;
+      }
+    }
+  }
+
+  // console.log(displayYear);
   return (
     <div style={{ paddingTop: "50px" }}>
       <Card style={{ padding: "20px" }}>
         <Card.Body>
           <div className="year">
-            <select onChange={handleChange}>
+            <select
+              onChange={(e) => setselectedYear(e.target.value)}
+              value={selectedYear}
+            >
+              <option value={currentYear} selected>
+                {currentYear}
+              </option>
               {year.reverse().map((year) => (
                 <option value={year}>{year}</option>
               ))}
             </select>
           </div>
           <CalendarHeatmap
-            startDate={selectedYear + "-01-01"}
-            endDate={selectedYear + "-12-31"}
+            startDate={displayYear + "-01-01"}
+            endDate={displayYear + "-12-31"}
             values={countData}
             classForValue={(value) => {
               if (!value) {
